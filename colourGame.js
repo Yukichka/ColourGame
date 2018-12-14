7773806524343c55b92f2345c9b69c50e77b5c06
Yukichka <yukiinsw@gmail.com>
2018-12-14 21:19:52 +0000

    fixed new colour button

--- colourGame.js
+++ colourGame.js
@@ -1,4 +1,5 @@
-var colors = generateRandomColors(6);
+var numSquares = 6;
+var colors = generateRandomColors(numSquares);
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
@@ -11,15 +12,34 @@ var hardBtn = document.getElementById("hardBtn");
 easyBtn.addEventListener("click", function(){
   hardBtn.classList.remove("selected")
   easyBtn.classList.add("selected")
+  numSquares = 3;
+  colors = generateRandomColors(numSquares);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+  for (var i = 0; i < squares.length; i++) {
+    if (colors[i]){
+      squares[i].style.backgroundColor = colors[i];
+    } else {
+        squares[i].style.display = "none";
+      }
+  }
 })
 
 hardBtn.addEventListener("click", function(){
   easyBtn.classList.remove("selected")
   hardBtn.classList.add("selected")
+  numSquares = 6;
+  colors = generateRandomColors(numSquares);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+  for (var i = 0; i < squares.length; i++) {
+      squares[i].style.backgroundColor = colors[i];
+      squares[i].style.display = "block";
+  }
 })
 
 resetButton.addEventListener("click",function(){
-  colors = generateRandomColors(6);
+  colors = generateRandomColors(numSquares);
   pickedColor = pickColor();
   colorDisplay.textContent = pickedColor;
   for (var i = 0; i < squares.length; i++) {
@@ -73,4 +93,4 @@ function randomColor(){
 	var g = Math.floor(Math.random() * 256);
 	var b = Math.floor(Math.random() * 256);
     return "rgb(" + r +", " + g +", "+ b +")";
-} 
+}
